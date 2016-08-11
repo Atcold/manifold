107bdb90d2d48d9a4ba358b7ef8f0855224aed8c
Alfredo Canziani <alfredo.canziani@gmail.com>
2016-08-11 11:49:49 -0400

    t-SNE Barnes-Hut for network's embedding

--- demos/demo_tsne.lua
+++ demos/demo_tsne.lua
@@ -30,13 +30,13 @@ end
 
 
 -- show map with original digits:
-local function show_map(method, mapped_data, X)
+local function show_map(method, mapped_data, input_data)
 
    -- draw map with original digits:
    local im_size = 2048
    local background = 0
    local background_removal = true
-   map_im = manifold.draw_image_map(mapped_data, X:resize(X:size(1), 1, 28, 28), im_size, background, background_removal)
+   map_im = manifold.draw_image_map(mapped_data, input_data, im_size, background, background_removal)
 
    -- plot results:
    image.display{image=map_im, legend=method, zoom=0.5}
@@ -54,19 +54,29 @@ local function demo_tsne()
    local mnist = require 'mnist'
    local testset = mnist.testdataset()
    testset.size  = N
-   testset.data  = testset.data:narrow(1, 1, N)
+   require 'cunn'
+   testset.data  = testset.data:narrow(1, 1, N):view(5000,1,28,28):cuda()
    testset.label = testset.label:narrow(1, 1, N)
-   local x = torch.DoubleTensor(testset.data:size()):copy(testset.data)
-   x:resize(x:size(1), x:size(2) * x:size(3))
+   require 'cudnn'
+   local net = torch.load('model.net')
+   net:evaluate()
+   net:remove(#net)
+   io.write('Computing the embedding...'); io.flush()
+   local x = torch.CudaTensor(5000, 128)
+   for i = 1, N do x[i] = net:forward(testset.data[i]) end
+   x = x:double()
+   print(' Done.')
+   --local x = torch.DoubleTensor(testset.data:size()):copy(testset.data)
+   --x:resize(x:size(1), x:size(2) * x:size(3))
    local labels = testset.label
 
    -- run t-SNE:
    local timer = torch.Timer()
-   opts = {ndims = 2, perplexity = 30, pca = 50, use_bh = false}
-   mapped_x1 = manifold.embedding.tsne(x, opts)
-   print('Successfully performed t-SNE in ' .. timer:time().real .. ' seconds.')
-   show_scatter_plot('t-SNE', mapped_x1, labels)
-   show_map('t-SNE', mapped_x1, x:clone())
+   -- opts = {ndims = 2, perplexity = 30, pca = 50, use_bh = false}
+   -- mapped_x1 = manifold.embedding.tsne(x, opts)
+   -- print('Successfully performed t-SNE in ' .. timer:time().real .. ' seconds.')
+   -- show_scatter_plot('t-SNE', mapped_x1, labels)
+   -- show_map('t-SNE', mapped_x1, testset.data:double():clone())
 
    -- run Barnes-Hut t-SNE:
    opts = {ndims = 2, perplexity = 30, pca = 50, use_bh = true, theta = 0.5}
@@ -74,7 +84,7 @@ local function demo_tsne()
    mapped_x2 = manifold.embedding.tsne(x, opts)
    print('Successfully performed Barnes Hut t-SNE in ' .. timer:time().real .. ' seconds.')
    show_scatter_plot('Barnes-Hut t-SNE', mapped_x2, labels)
-   show_map('Barnes-Hut t-SNE', mapped_x2, x:clone())
+   show_map('Barnes-Hut t-SNE', mapped_x2, testset.data:double():clone())
 end
 
 
